a4877bf3949877fe89dba3406d74bd82461b2ada
Krishna Vijay Samayamantri <svrbasky@gmail.com>
2020-09-23 23:58:36 -0700

    Webcam Vitals Tracker Source Initiated.

---
+++ flask_vitals_tracker_webpage.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for
+
+# Define app
+app = Flask(__name__)
+
+
+# Create Webpage that displays vitals
+@app.route('/')
+@app.route('/vitals')
+def vitals():
+	return render_template('vitals.html', title='Webcam Vitals Tracker')
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
