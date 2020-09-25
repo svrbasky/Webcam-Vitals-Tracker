61263661efaa0d415bffcf300fd5fe9e56d0ff83
Krishna Vijay Samayamantri <svrbasky@gmail.com>
2020-09-24 20:49:20 -0700

    Added Dummy Pages for Page navigation.

--- flask_vitals_tracker_webpage.py
+++ flask_vitals_tracker_webpage.py
@@ -5,10 +5,38 @@ app = Flask(__name__)
 
 
 # Create Webpage that displays vitals
+# Homepage
 @app.route('/')
+@app.route('/home')
 @app.route('/vitals')
 def vitals():
 	return render_template('vitals.html', title='Webcam Vitals Tracker')
+
+# Dashboard
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', title = 'Dashboard')
+
+# Description
+@app.route('/description')
+def description():
+    return render_template('description.html', title = 'Description')
+
+# About
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'About')
+
+# Updates
+@app.route('/updates')
+def updates():
+    return render_template('updates.html', title = 'Updates')
+
+# Contact
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title = 'Contact')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
