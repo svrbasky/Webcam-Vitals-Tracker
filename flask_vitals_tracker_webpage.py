d6c79b46c370b1ba67aa0ac2c7a3b9ff59864694
Krishna Vijay Samayamantri <svrbasky@gmail.com>
2020-09-25 15:54:10 -0700

    Navbar created.

--- flask_vitals_tracker_webpage.py
+++ flask_vitals_tracker_webpage.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/vitals')
 def vitals():
-	return render_template('layout.html')
+	return render_template('vitals.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
